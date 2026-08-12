044aef4d9c3722e504f0590b0a01af5364ed2b6a
Aaditya2310 <adityadpatil.23@gmail.com>
2026-08-12 12:12:13 +0530

    remove

--- cookies-banner/src/Component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Component() {
-  return (
-    <div style={{ padding: '1em', background: '#eee', textAlign: 'center' }}>
-      this is component
-    </div>
-  );
-}
